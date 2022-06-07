5366d9190720be8c65a608cb8117b5f3b5ecda08
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-07 20:31:27 +0300

    updated

--- Variables/index.js
+++ Variables/index.js
@@ -1,4 +1,5 @@
 
+// var bir blokla sınırlı değil.
 var item1 = "var variable - Global";
 
 if(true)
@@ -7,6 +8,7 @@ if(true)
     document.write(item1);
 }
 document.write(', ' + item1+ '<br>');
+document.write('(var bir blokla sınırlı değil. let ve const tam ters.) <br><br>')
 
 
 // Şimdi let ile:
@@ -29,3 +31,47 @@ if(true)
     document.write(item3);
 }
 document.write(', ' + item3+ '<br>');
+
+// ****************************************************
+
+var name1 = "Sam";
+name1 = "Alex";
+var name1 = "Alex";
+console.log(name1, 'Var');
+
+
+let name2 = "Sam";
+name2 = "Alex";
+//let name2 = "Alex"; // HATA
+console.log(name2, 'Let');
+
+
+const name3 = "Sam";
+//name3 = "Alex"; // HATA
+console.log(name3, 'Const');
+
+// **************************************************** Typeof
+
+var oneObj = {};
+var oneArr = [1,2,'name'];
+// ikisini typeof object'tir.
+console.log(oneObj, typeof oneObj);
+console.log(oneArr, typeof oneArr);
+
+// **************************************************** Length
+
+console.log(oneArr.length); // 3
+var aName = "Alex Ferguson";
+console.log(aName.length);  // 13
+
+// **************************************************** Eval
+
+console.log(eval("10 * 4"));    // 40
+
+function Deneme(){
+    console.log("running function deneme()");
+}
+var result = "Deneme()";
+console.log(eval(result));  // running function deneme()
+
+// **************************************************** 
