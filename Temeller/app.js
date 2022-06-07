5b305b4536c9a6788b3f4eb91c3f43f8b2690f58
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-07 12:34:16 +0300

    first proj done!

--- Temeller/app.js
+++ Temeller/app.js
@@ -1,1 +1,13 @@
-console.log("Hey");
+var x = 12;
+console.log(typeof x, "'debug point'");
+
+// Uyarı vermek için:
+alert("uyarı");
+
+// Direk sayfada yazmak:
+document.write("<br />bir şey js tarafından.");
+
+// Sayfada bir şeyi değişmek
+document.getElementById("Deneme2").innerHTML = "Altered to Hello! by JS";
+
+debugger;   // debuggeri açiliyor.
