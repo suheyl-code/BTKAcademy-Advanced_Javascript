2dadecb3d448c9079a3dce9432b030e4de2fb5ac
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-06-08 21:16:12 +0300

    ..

--- Variables/index.js
+++ Variables/index.js
@@ -32,7 +32,7 @@ if(true)
 }
 document.write(', ' + item3+ '<br>');
 
-// ****************************************************
+// *********************************************************
 
 var name1 = "Sam";
 name1 = "Alex";
@@ -50,7 +50,7 @@ const name3 = "Sam";
 //name3 = "Alex"; // HATA
 console.log(name3, 'Const');
 
-// **************************************************** Typeof
+// ********************************************************* Typeof
 
 var oneObj = {};
 var oneArr = [1,2,'name'];
@@ -58,13 +58,13 @@ var oneArr = [1,2,'name'];
 console.log(oneObj, typeof oneObj);
 console.log(oneArr, typeof oneArr);
 
-// **************************************************** Length
+// ********************************************************* Length
 
 console.log(oneArr.length); // 3
 var aName = "Alex Ferguson";
 console.log(aName.length);  // 13
 
-// **************************************************** Eval
+// ********************************************************* Eval
 
 console.log(eval("10 * 4"));    // 40
 
@@ -74,16 +74,35 @@ function Deneme(){
 var result = "Deneme()";
 console.log(eval(result));  // running function deneme()
 
-// **************************************************** Slice
+// ********************************************************* Slice
 var sliceDeneme = "this is a string literal";
 console.log(sliceDeneme.slice(10));     // string literal
 console.log(sliceDeneme.slice(12,17));  //ring
 
-// **************************************************** Substr
+// ********************************************************* Substr
 
 console.log("bu bir metin dir.".substr(11));    // n dir.
 // second argument is the number of characters forward
 console.log("bu bir metin dir.".substr(10,3));    // in
 console.log("bu bir metin dir.".substr(-4));    // dir.
 
-// ****************************************************
+// ********************************************************* Repeat
+
+console.log("Hello".repeat(2)); //HelloHello
+
+// ********************************************************* ToString / String
+
+var aNumber = 2;
+console.log(aNumber.toString(), 
+            typeof aNumber.toString()); // 2 string
+
+console.log(String(aNumber), 
+            typeof String(aNumber));    // 2 string
+
+// ********************************************************* Number
+
+console.log(Number(true), 
+            Number(false), 
+            Number(new Date()));    // 1 0 1654710913140
+
+// *********************************************************
